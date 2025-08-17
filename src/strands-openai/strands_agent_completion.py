3925aa588b52911c16b3ffe063692ba8ef8554a0
renya <rsmilek5492@gmail.com>
2025-08-17 23:39:19 +0900

    refactor

--- src/strands-openai/strands_agent_completion.py
+++ src/strands-openai/strands_agent_completion.py
@@ -4,24 +4,32 @@ from dotenv import load_dotenv
 from strands import Agent
 from strands.models.openai import OpenAIModel
 
+MODEL_ID = "openai.gpt-oss-20b-1:0"
 load_dotenv(override=True)
 
-model = OpenAIModel(
-    client_args={
-        "base_url": "https://bedrock-runtime.us-west-2.amazonaws.com/openai/v1",
-        "api_key": os.getenv("AWS_BEARER_TOKEN_BEDROCK"),
-    },
-    # **model_config
-    model_id="openai.gpt-oss-20b-1:0",
-    params={
-        "max_completion_tokens": 1000,
-        "temperature": 0.7,
-        "reasoning_effort": "low",
-    },
-)
 
-agent = Agent(
-    model=model,
-    system_prompt="質問に対して日本語で回答してください。必ず論理的に思考せよ．",
-)
-agent("3.11と3.9はどちらが大きいですか？")
+def main() -> None:
+    model = OpenAIModel(
+        client_args={
+            "base_url": "https://bedrock-runtime.us-west-2.amazonaws.com/openai/v1",
+            "api_key": os.getenv("AWS_BEARER_TOKEN_BEDROCK"),
+        },
+        # **model_config
+        model_id=MODEL_ID,
+        params={
+            "max_completion_tokens": 1024,
+            "temperature": 1.0,
+            "top_p": 1.0,
+            "reasoning_effort": "medium",
+        },
+    )
+
+    agent = Agent(
+        model=model,
+        system_prompt="質問に対して日本語で回答してください。必ず論理的に思考せよ．",
+    )
+    agent("3.11と3.9はどちらが大きいですか？")
+
+
+if __name__ == "__main__":
+    main()
